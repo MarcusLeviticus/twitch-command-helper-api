ef1498dd9d7e47bec1afffdbdf278af73b908cba
Marviuz <marviuzmarvieluz@gmail.com>
2022-05-11 18:48:22 +0800

    Switch from vercel to github

--- src/index.js
+++ src/index.js
@@ -10,38 +10,36 @@ app.get('/', (req, res) => {
 });
 
 // !so !shoutout
-// app.get('/shoutout/:name/:game', (req, res) => {
-//   const { name, game } = req.params;
+app.get('/shoutout/:name/:game', (req, res) => {
+  const { name, game } = req.params;
 
-//   if (game.trim() === '<no game>') res.send(`HOW IS YOU MADAFA- I mean ${name}? HungryPaimon`);
-//   else res.send(`Checkout ${name} at https://twitch.tv/${name} Last seen playing ${game} last stream! HungryPaimon`);
-// });
+  if (game.trim() === '<no game>') res.send(`HOW IS YOU MADAFA- I mean ${name}? HungryPaimon`);
+  else res.send(`Checkout ${name} at https://twitch.tv/${name} Last seen playing ${game} last stream! HungryPaimon`);
+});
 
-// // !clip
-// app.get('/clip/:code/:channel/:dcServer/:dcChannel/:clipper', async (req, res) => {
-//   const { code, channel, dcServer, dcChannel, clipper } = req.params;
-//   const webhook = `https://discord.com/api/webhooks/${dcServer}/${dcChannel}`;
+// !clip
+app.get('/clip/:code/:channel/:dcServer/:dcChannel/:clipper', async (req, res) => {
+  const { code, channel, dcServer, dcChannel, clipper } = req.params;
+  const webhook = `https://discord.com/api/webhooks/${dcServer}/${dcChannel}`;
 
-//   try {
-//     const clipURL = await createClip(code, channel);
+  try {
+    const clipURL = await createClip(code, channel);
 
-//     shareToDiscord(clipURL, webhook, clipper)
-//       .then(() => {
-//         console.log('Discord share successfull');
-//         res.send(`${clipData.url} also shared to discord!`);
-//       })
-//       .catch(err => {
-//         console.error('shareToDiscord thenable Something went wrong');
-//         res.send('Discord share failed');
-//         throw new Error(err);
-//       });
-//   } catch (err) {
-//     console.error('app.get FAILED');
-//     res.send(`failed to share to discord server D:`);
-//     throw new Error(err);
-//   }
-// });
+    shareToDiscord(clipURL, webhook, clipper)
+      .then(() => {
+        console.log('Discord share successfull');
+        res.send(`${clipData.url} also shared to discord!`);
+      })
+      .catch(err => {
+        console.error('shareToDiscord thenable Something went wrong');
+        res.send('Discord share failed');
+        throw new Error(err);
+      });
+  } catch (err) {
+    console.error('app.get FAILED');
+    res.send(`failed to share to discord server D:`);
+    throw new Error(err);
+  }
+});
 
 app.listen(PORT, () => console.log(`> Ready on port ${PORT}`));
-
-// module.exports = app;
