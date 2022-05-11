71fba51f5f25d8f4271f9289c3a182336260323c
Marviuz <marviuzmarvieluz@gmail.com>
2022-05-11 17:23:12 +0800

    Initial commit

---
+++ src/utils/clip.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const createClip = async (code, channel) => {
+  try {
+    const { data: clipURL } = await axios({
+      method: 'get',
+      url: `https://api.thefyrewire.com/twitch/clips/create/${code}?channel=${channel}`
+    });
+
+    return clipURL;
+  } catch (err) {
+    console.error('createClip ERROR');
+    throw new Error(err);
+  }
+};
+
+const shareToDiscord = async (clip, webhook, clipper) => {
+  try {
+    return axios({
+      method: 'post',
+      url: webhook,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data: JSON.stringify({ content: `${clipper} clipped ${clip}` })
+    });
+  } catch (err) {
+    console.error('shareToDiscord ERROR');
+    throw new Error(err);
+  }
+};
+
+exports.createClip = createClip;
+exports.shareToDiscord = shareToDiscord;
